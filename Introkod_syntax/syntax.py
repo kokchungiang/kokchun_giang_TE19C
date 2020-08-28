9ef161e73c8cd3df858a5d83999a461c68066adb
Kokchun <69901510+kokchungiang@users.noreply.github.com>
2020-08-28 09:38:29 +0200

    lagt till kommentar

--- Introkod_syntax/syntax.py
+++ Introkod_syntax/syntax.py
@@ -12,4 +12,4 @@ print("Ditt namn är",name)
 side = float(input("Ange kvadratens sida: ")) 
 area = side**2
 
-print(f"Arean av kvadraten är {area} a.e.")
+print(f"Arean av kvadraten är {area} a.e.") # print
